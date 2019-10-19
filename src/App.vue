7f79d3f3f978bd1009c39a9e0ebeb39285452456
Muneeb Rehman <muneebrehman1992@gmail.com>
2019-10-20 01:40:46 +0500

    RobotBuilder to build robot and add cart

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="../src/assets/build-a-robot-logo.png" />
+            Build-a-Robot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <HomePage />
+      <RobotBuilder />
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    // HomePage,
+    RobotBuilder,
   },
 };
 </script>
@@ -31,5 +43,24 @@ main {
   background-color:white;
   width: 1024;
   min-width: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
